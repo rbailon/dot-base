d56f6c4546ce52cdc868d83137235715a34853cd
Ricardo Bailón <rbailon@gmail.com>
2026-03-12 18:12:44 +0000

    [Fix] Adaptación nueva versión y otros

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,15 +9,16 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls"	}
+				ensure_installed = { "lua_ls" }
 			})
 		end
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function ()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
+			-- local lspconfig = require("lspconfig")
+			vim.lsp.enable("lua_ls")
+			-- lspconfig.lua_ls.setup({})
 		end
 	}
 }
